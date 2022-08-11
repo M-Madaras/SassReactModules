5ac9611d9a1851a412a094cda30fc553c861fe28
“Mason <masonmadaras@gmail.com>
2022-08-11 17:01:20 -0400

    sass-css-react-modules

---
+++ src/components/Header.jsx
@@ -0,0 +1,28 @@
+import styles from './Header.module.scss'
+
+const Header = () => {
+    return (
+        <>
+            <ul className="navigation">
+                <li><a href="">Home</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Contact</a></li>
+                <li><a href="">Links</a></li>
+            </ul>
+
+            <div className="menu">
+                <a href="" className="menu__link">Link 1</a>
+                <a href="" className="menu__link">Link 2</a>
+                <a href="" className="menu__link">Link 3</a>
+            </div>
+
+
+            <div className='mod'>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti voluptatum officia ipsam earum eius optio similique enim, laborum veniam et magni quasi natus commodi provident qui nam dolores sit molestias.</p>
+            </div>
+
+        </>
+    )
+}
+
+export default Header;
